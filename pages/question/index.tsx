d2d1ee0b595e60f84f8614081755a582d7e93bba
terry <s.3.h.st.sp211@gmail.com>
2024-03-03 13:19:13 +0900

    question pageを仮で作ってみた

---
+++ pages/question/index.tsx
@@ -0,0 +1,10 @@
+import {NextPage} from "next";
+import Layout from "../../components/Layout";
+
+const QuestionPage: NextPage = () => (
+  <Layout title="Question | 質問">
+      <div>質問</div>
+  </Layout>
+);
+
+export default QuestionPage
